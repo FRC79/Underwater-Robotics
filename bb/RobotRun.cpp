3823945c5cbb5cb7a17cf7e9542491be6585376c
Sebastian Lopez-Cot <slcot@mitre.org>
2013-07-13 13:33:30 -0400

    Added SimpleGPIO libs and imported it into RobotRun

--- bb/RobotRun.cpp
+++ bb/RobotRun.cpp
@@ -1,4 +1,5 @@
 #include "RobotRun.h"
+#include "gpio\SimpleGPIO.h"
 
 RobotRun::RobotRun() { }
 
@@ -12,6 +13,7 @@ void RobotRun::setup()
 bool RobotRun::loop() 
 {
 	// Loop function (runs iteratively)
+	
 
 	return true; // true: we want to keep looping
 				 // false: we want to terminate loop
